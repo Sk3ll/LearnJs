3764b689c986784049aa131eaed4ef9856c8336b
Sk3ll <sk3llyar@gmail.com>
2019-11-24 13:10:11 +0200

    прототипы и наследование
    переделал прототипы и сделал наследование и исключения

--- Prototype.js
+++ Prototype.js
@@ -7,10 +7,10 @@
 /*
 function Player(name) {
     this.name = name;
+    this.playPause = playPause;
 }
 
 Player.prototype.switcher = function (playPause) {
-    this.playPause = playPause;
     if (this.playPause == 0){
         console.log("switch off " + this.name);
     }
@@ -32,22 +32,27 @@ iPod.switcher(0);
     Громкость может быть в диапазоне от 0 до 100.
 */
 /*
-function Player(volume) {
-    this.__volume = volume;
-    if (this.__volumeValid(volume)) {
-        this.__volume = volume;
-        }
+function Player() {
+    this.__volume ;
 }
 
 Player.prototype.getVolume = function () {
     console.log(this.__volume);
 };
 
-Player.prototype.setVolume = function() {
-    if (this.__volumeValid(volume)) {
-        this.__volume = volume;
-        }
+Player.prototype.setVolume = function(value) {
+    if (typeof value == "number" && !isNaN(value)){
+        this.__volume = value;
+    }
+    else console.log("Введите цифры");
 };
+/////// Единый геттер-сеттер
+Player.prototype.volume = function (volume) {
+        if (volume === undefined) { 
+            return this.__volume; 
+        } else 
+            this.__volume = volume;
+        }
 
 Player.prototype.displayInfo = function () {
     if (this.__volume > 0 && this.__volume <= 100) {
@@ -58,24 +63,7 @@ Player.prototype.displayInfo = function () {
     }
 };
 
-Player.prototype.__volumeValid = function (volume) {
-    if (volume > 0 && volume <= 100) {
-        return true;
-    } else {
-        return false;
-        }
-    };
-Player.prototype.volume = function (volume) {
-        if (volume === undefined) { 
-            return this.__volume; 
-        } else {
-            if (this.__volumeValid(volume)) {
-                this.__volume = volume;
-            }
-        }
-};
-var volumePlayer = new Player(-2);
-volumePlayer.displayInfo ();
+var p = new Player();
 
 /*
     Задание №2.
@@ -91,15 +79,14 @@ __firstNumber и __secondNumber;
 вызываться метод __calc(). Получается, что при успешной установке либо
 __firstNumber либо __secondNumber, происходит перерасчет result.
 */
-
+/*
 function Sum() {
     this. __firstNumber = 0;
     this.__secondNumber = 0;
     this.result = 0;
 }
-Sum.prototype.__calc = function (__firstNumber, __secondNumber) {
-    this.result = __firstNumber + __secondNumber;
-    console.log(this.result)
+Sum.prototype.__calc = function () {
+    this.result = this. __firstNumber + this.__secondNumber;
 }
 
 Sum.prototype.firstNumber = function (value) {
@@ -107,26 +94,316 @@ Sum.prototype.firstNumber = function (value) {
         return this.__firstNumber;
         
     } else {
-        this.__firstNumber = value;
+        if(typeof value == "number" && !isNaN(value)) {
+            this.__firstNumber = value;
+            this.__calc();
+        }
+        else console.log("введите цифры");
     }
 }
+
 
 Sum.prototype.secondNumber = function(value) {
     if (this.__secondNumber === undefined) {
         return this.__secondNumber;
     }else {
-        this.__secondNumber = value;
+        if(typeof value == "number" && !isNaN(value)) {
+            this.__secondNumber = value;
+            this.__calc();
+        }
+        else console.log("введите цифры")
     }
 }
+var c = new Sum ();
 
-Sum.prototype.__isValidNumber = function (value) {
-    if (typeof value == "Number" && !isNaN(value)) {
-        return true;
+
+//////////////////НАСЛЕДОВАНИЕ
+/*
+        Задание №1.
+    Создайте тип объектов Геометрическая фигура, определите в нем приватные
+    свойства для задания центра фигуры. Для доступа к этим свойствам определите
+    необходимые метод/методы.
+    На основе этого типа создайте два новых типа (наследники) – Прямоугольник и
+    Круг. В этих типах определите особые приватные свойства. (Например, длина
+    диагонали для Прямоугольника и радиус для Круга). В каждый из типов добавьте
+    публичный метод info(), который выводит на экран всю доступную информацию о
+    фигуре.
+*/
+/*
+function GeomFigure (x, y) {
+    this.__x = 0;
+    this.__y = 0;
+}
+
+GeomFigure.prototype.getX = function (x) {
+    return this.__x ;
+}
+
+GeomFigure.prototype.setX = function (value) {
+    if (typeof value == "number" && !isNaN(value)) {
+        this.__x = value;
     }
     else {
-        return false;
+        console.log("Введите число");
     }
 }
 
-var c = new Sum ();
-c.__calc(3,4);
+GeomFigure.prototype.getY = function (y) {
+    return this.__y ;
+}
+
+GeomFigure.prototype.setY = function (value) {
+    if (typeof value == "number" && !isNaN(value)) {
+        this.__y = value;
+    }
+    else {
+        console.log("Введите число");
+    }
+}
+
+function Rectangle(x, y){
+    GeomFigure.call(this, x, y)
+    this.__diagonal = 0;
+}
+
+Rectangle.prototype = Object.create(GeomFigure.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.getDiagonal = function (diagonal) {
+    return this.__diagonal ;
+}
+
+Rectangle.prototype.setDiagonal = function (value) {
+    if (typeof value == "number" && !isNaN(value)) {
+        this.__diagonal = value;
+    }
+    else {
+        console.log("Введите число");
+    }
+}
+
+Rectangle.prototype.info = function() {
+    console.log("Диагональ: " + this.__diagonal);
+}
+
+function Circle(x, y) {
+    GeomFigure.call(this, x, y)
+    this.__radius = 0;
+}
+
+Circle.prototype = Object.create(Circle.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.getRadius = function (radius) {
+    return this.__radius ;
+}
+
+Circle.prototype.setRadius = function (value) {
+    if (typeof value == "number" && !isNaN(value)) {
+        this.__radius = value;
+    }
+    else {
+        console.log("Введите число");
+    }
+}
+
+Circle.prototype.info = function() {
+    console.log("Диагональ: " + this.__radius);
+}
+
+
+var c = new GeomFigure();
+var d = new Rectangle();
+var r = new Circle();
+
+/*
+        Задание №2. Полиморфизм
+    Создайте тип объектов Квадрат, определите защищенное свойство для
+    хранения значения стороны. Для доступа к этому свойству определите необходимые
+    метод/методы. Также определите публичный метод, который возвращает периметр.
+    На основе этого типа создайте тип Куб и переопределите метод получения
+    периметра.
+    Реализуйте 2 версии текущего задания, используя следующие способы
+    переопределения:
+    1. Без расширения (полностью переопределить метод родителя);
+    2. С расширением (использовать результат метода родителя для дальнейших
+    расчетов).
+*/
+/*
+function Squre(side){
+    this._side = side;
+   
+}
+
+Squre.prototype.getSide = function (side) {
+    return this._side ;
+}
+
+Squre.prototype.setSide = function (value) {
+    if (typeof value == "number" && !isNaN(value)) {
+        this._side = value;
+    }
+    else {
+        console.log("Введите число");
+    }
+}
+
+Squre.prototype.perimeter = function(){
+    
+    return this._side * 4;
+}
+/////////// 1
+function Cube1 (side){
+    Squre.call(this, side);
+}
+
+Cube1.prototype = Object.create(Squre.prototype);
+Cube1.prototype.constructor = Cube1;
+
+Cube1.prototype.perimeter = function() {
+    return this._side * 12;
+}
+
+/////////2 
+function Cube2 (side){
+    Squre.call(this, side);
+}
+
+Cube2.prototype = Object.create(Squre.prototype);
+Cube2.prototype.constructor = Cube2;
+
+Cube2.prototype.perimeter = function() {
+    return Squre.prototype.perimeter.call(this) * 3;
+
+}
+
+var s = new Squre(2);
+var c1 = new Cube1(2);
+var c2 = new Cube2(2);
+
+/*
+   Задание №1.
+Напишите код для "безопасного" вычисления периметра и площади
+прямоугольника в виде модуля.
+Сторонами прямоугольника может быть только число (тип данных Number,
+значения других типов данных нельзя устанавливать), значение которого, строго
+больше 0. 
+*/
+/*
+var paymentP = (function () {
+    var firsSide;
+    var secondSide;
+    function operation() {
+        return (firsSide + secondSide) * 2;
+    }
+    return {
+        setFirstSide: function (value) {
+            if(typeof value == "number" && !isNaN(value) && value > 0){
+                firsSide = value;
+            } else {
+                console.log("Введите число больше 0");
+            }
+        },
+        setSecondSide: function (value) {
+            if(typeof value == "number" && !isNaN(value) && value > 0){
+                secondSide = value;
+            } else {
+                console.log("Введите число больше 0");
+            }
+        },
+        operation: function () {
+            return operation ()
+        }
+    }
+    
+    }());
+
+var paymentS = (function () {
+    var firsSide;
+    var secondSide;
+    function operation() {
+        return firsSide * secondSide;
+    }
+    return {
+        setFirstSide: function (value) {
+            if(typeof value == "number" && !isNaN(value) && value > 0){
+                firsSide = value;
+            } else {
+                console.log("Введите число больше 0");
+            }
+        },
+        setSecondSide: function (value) {
+            if(typeof value == "number" && !isNaN(value) && value > 0){
+                secondSide = value;
+            } else {
+                console.log("Введите число больше 0");
+            }
+        },
+        operation: function () {
+            return operation ()
+        }
+    }
+    
+}());
+
+
+
+/*
+    Задание №1.
+Напишите функцию sumSliceArray(arr, first, second), которая принимает массив
+arr и два числа (first и second) – порядковые номера элементов массива, которые
+необходимо суммировать. Например, если ввели 3 и 5 – суммируются 3-й и 5-й
+элементы.
+Функция должна генерировать исключения если были введены не числа, и
+когда одно из чисел, или оба больше размера массива.
+Напишите код, который использует данную функцию, предусмотрите
+обработку возможных исключений.
+*/
+
+function sumSliceArray(arr, first, second){
+var arrIsArr = false;
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == "number" && !isNaN(arr[i])) {
+        arrIsArr = true;
+        }
+        else {
+        arrIsArr = false;
+        break;
+        }
+    }
+
+    if (arrIsArr && typeof first == "number" && !isNaN(first) && first<=arr.length && typeof second == "number" && !isNaN(second) && second<=arr.length) {
+        return arr [first - 1] + arr [second - 1];
+    }
+    else return "введите числа";
+}
+
+//console.log(sumSliceArray([1,2,3,4],5,3))
+
+
+//try catch
+
+function sumSliceArray(arr, first, second){
+    var arrIsArr = false;
+        for (var i = 0; i < arr.length; i++) {
+            if (typeof arr[i] == "number" && !isNaN(arr[i])) {
+            arrIsArr = true;
+            }
+            else {
+            arrIsArr = false;
+            break;
+            }
+        }
+    
+        if (arrIsArr && typeof first == "number" && !isNaN(first) && first<=arr.length && typeof second == "number" && !isNaN(second) && second<=arr.length) {
+            return arr [first - 1] + arr [second - 1];
+        }
+        else throw new Error ("введите числа");
+    }
+    
+try {    
+    console.log(sumSliceArray([1,2,3,4],5,3))
+}
+catch(error) {
+    console.log(error);
+}
